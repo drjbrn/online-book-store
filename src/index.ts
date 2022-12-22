831d8ca274a4d3a01c9ef772c2c794234d7062d3
Rimfis <dimitri.rim@yandex.ru>
2022-12-22 18:06:33 +0300

    feat: add page 404

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import './index.html';
 import './index.scss';
 import Router from './modules/Router/Router';
+import NoPage from './components/NoPage/index';
 
+const noPage = new NoPage();
 const router = new Router();
 
+router.addRoutes({ '404': noPage.render });
 router.init();
